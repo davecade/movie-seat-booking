f18e0202ebd888f3e8c5b73ebf603da43d466ce2
Dave <dave.cadelina@outlook.com>
2020-10-01 22:50:50 +1000

    added JS functionality

---
+++ app.js
@@ -0,0 +1,32 @@
+const movie = document.getElementById('movies')
+const totalSeats = document.getElementById('amount-selected')
+const totalCost = document.getElementById('cost')
+const selectedSeat = document.querySelector('.seat-container')
+let movieCost = movie.value
+let amountOfSeatsSelected = 0;
+
+function displayCost(cost) {
+    totalCost.innerHTML = cost
+}
+
+selectedSeat.addEventListener('click', (e) => {
+    if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
+        if(e.target.classList.contains('selected')) {
+            movieCost = movie.value
+            e.target.classList.remove('selected')
+            amountOfSeatsSelected--
+        } else {
+            movieCost = movie.value
+            e.target.classList.add('selected')
+            amountOfSeatsSelected++;
+        }
+        totalSeats.innerHTML = amountOfSeatsSelected;
+        displayCost(`$${movieCost*amountOfSeatsSelected}`)
+    }
+    
+})
+
+movie.addEventListener('change', ()=> {
+    movieCost = movie.value;
+    displayCost(`$${movieCost*amountOfSeatsSelected}`)
+})
